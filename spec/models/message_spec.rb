137a3b36f87ce80d1e41255b99d13da4881a8811
David Elentok <3david@gmail.com>
2012-07-03 20:24:39 +0300

    using fabrication instead of User.create

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe Message do
   describe "#to_user" do
     it "returns all of the message sent to a user" do
-      user = User.create username: '456', 
-        password: '123456', password_confirmation: '123456'
+      user = Fabricate(:user)
       message1 = Message.create
       message2 = Message.create
       message1.recipients << user
