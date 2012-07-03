562d64f8a8e56f8925c91f9d4463bb9f021ed0bc
David Elentok <3david@gmail.com>
2012-07-03 17:47:27 +0300

    added support for selenium tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -51,12 +51,12 @@ Spork.prefork do
   # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
   # See the DatabaseCleaner documentation for details. Example:
   #
-  #   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  #     # { :except => [:widgets] } may not do what you expect here
-  #     # as tCucumber::Rails::Database.javascript_strategy overrides
-  #     # this setting.
-  #     DatabaseCleaner.strategy = :truncation
-  #   end
+  Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
+    # { :except => [:widgets] } may not do what you expect here
+    # as tCucumber::Rails::Database.javascript_strategy overrides
+    # this setting.
+    DatabaseCleaner.strategy = :truncation
+  end
   #
   #Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
      #DatabaseCleaner.strategy = :transaction
